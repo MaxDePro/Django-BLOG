f4312492ee536393b9ba330e7ceccee639589ba3
MaxDePro <xmaksg@gmail.com>
2022-08-23 14:00:29 +0300

    make templates for create posts/tags and realize functionality to add posts and tags, TAGFORM and POSTFORM created

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,59 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from .models import *
 
-def post_list(request):
-    return HttpResponse('HELLO WORLD')
+from django.views.generic import View
+from .utils import *
+from .forms import *
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class TagCreate(ObjectCreateMixin, View):
+    form = TagForm
+    template = 'blog/tag_create.html'
+    # def get(self, request):
+    #     form = TagForm()
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #
+    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class PostCreate(ObjectCreateMixin, View):
+    form = PostForm
+    template = 'blog/post_create.html'
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create.html', context={'form': bound_form})
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
